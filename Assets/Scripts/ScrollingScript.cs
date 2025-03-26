3df0f5819df8319dae13ab90a7277415c39d8173
NoiseMak12 <M.ANISJKO-2023@hull.ac.uk>
2025-03-26 18:36:18 +0000

    Scrolling works! (in one direction)
    Expect it to scroll to the right, you can also scroll up but don't expect the texture to change back. got a headache and still can't solve. pls help :(

--- Assets/Scripts/ScrollingScript.cs
+++ Assets/Scripts/ScrollingScript.cs
@@ -5,49 +5,45 @@ using UnityEngine;
 public class ScrollingScript : MonoBehaviour
 {
     public float speed;
-    public float swapDistance = 10f; // Distance threshold for swapping materials
     public bool isMoving = true;
+    public bool scrollUp = false; // New variable to control the direction of scrolling
+    public Material secondMaterial; // New variable for the second material
+    public float blendSpeed = 0.5f; // Speed of blending between materials
 
-    [SerializeField]
-    private Renderer bgRenderer;
+    private float offset;
+    private Material material;
+    private float blendFactor = 0.0f;
 
-    [SerializeField]
-    private Material material1;
-
-    [SerializeField]
-    private Material material2;
-
-    private bool useMaterial1 = true;
-    private float scrolledDistance = 0f;
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        material.SetTexture("_SecondTex", secondMaterial.GetTexture("_MainTex"));
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (isMoving)
         {
-            float offset = speed * Time.deltaTime;
-            bgRenderer.material.mainTextureOffset += new Vector2(offset, 0);
-            scrolledDistance += offset;
-
-            if (scrolledDistance >= swapDistance)
+            if (scrollUp)
             {
-                SwapMaterials();
-                scrolledDistance = 0f; // Reset the scrolled distance after swapping
+                offset += (speed * Time.deltaTime);
+                // this solution is not perfect, but it works for now
+                // sets the texture of material with second material texture
+                // but attempting to change back is kind of impossible
+                material.SetTexture("_MainTex", secondMaterial.GetTexture("_MainTex"));
+                material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+                blendFactor = Mathf.Clamp01(blendFactor + blendSpeed * Time.deltaTime);
             }
-        }
-    }
+            else
+            {
+                offset += (speed * Time.deltaTime);
+                material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+                blendFactor = Mathf.Clamp01(blendFactor - blendSpeed * Time.deltaTime);
+            }
 
-    public void SwapMaterials()
-    {
-        if (useMaterial1)
-        {
-            bgRenderer.material = material2;
+            material.SetFloat("_Blend", blendFactor);
         }
-        else
-        {
-            bgRenderer.material = material1;
-        }
-        useMaterial1 = !useMaterial1;
     }
 }
 
