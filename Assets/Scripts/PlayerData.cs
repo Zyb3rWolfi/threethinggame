98b547c3d6ad92edafd83f57ed0609cbe19a95e0
Zyb3rWolfi <zyb3rwolfi@proton.me>
2025-03-29 14:56:44 +0000

    Started work on UI

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "player-data", menuName = "Player/Create Player Data", order = 3)]
+
+public class PlayerData : ScriptableObject
+{
+    public string playerName;
+    public DrinkSerializable[] unlockedDrinks;
+}
